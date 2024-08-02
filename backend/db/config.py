75002fd916da1e757f84a41baf67f85f3f427e1f
abhinavbichal <65791148+thabat12@users.noreply.github.com>
2024-08-02 01:26:05 -0500

    coded the basic format/ structure for database unit tests

--- backend/db/config.py
+++ backend/db/config.py
@@ -1,5 +1,11 @@
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    TEST_SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    TEST_SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_SQLALCHEMY_DATABASE_URI')
+    TEST_PLAID_URL = os.environ.get('TEST_PLAID_URL')
+    TEST_PLAID_CLIENT_ID = os.environ.get('TEST_PLAID_CLIENT_ID')
+    PLAID_SECRET = os.environ.get('PLAID_SECRET')
--- backend/db/test_models.py
+++ backend/db/test_models.py
@@ -1,20 +1,72 @@
 import os
-from dotenv import load_dotenv
 import requests
 import unittest
+from datetime import datetime
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+
 from db.models import User, Account, Merchant, Transaction, Subscription
 from db.config import Config
 
-load_dotenv()
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = Config.TEST_SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-TEST_SQLALCHEMY_DATABASE_URI = os.getenv('TEST_SQLALCHEMY_DATABASE_URI')
+sandbox_url = Config.TEST_PLAID_URL
+client_id = Config.TEST_PLAID_CLIENT_ID
+secret = Config.PLAID_SECRET
+standard_header = {'Content-Type': 'application/json'}
+
 
 class TestTableCreationOperations(unittest.TestCase):
-    def setUp(self):
-        pass
+    @classmethod
+    def setUpClass(cls):
+        cls.app = app
+        cls.app_context = cls.app.app_context()
+        cls.app_context.push()
+        cls.client = cls.app.test_client()
 
-    def test_create_models(self):
-        pass
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.app_context.pop()
+
+    def setUp(self):
+        db.create_all()
+
+    def test_login_user(self):
+        response = requests.post(f'{sandbox_url}/sandbox/public_token/create',
+            headers=standard_header,
+            json={'client_id': client_id, 'secret': secret,
+                    'institution_id': 'ins_20',
+                    'initial_products': ['transactions'],
+                    'options': {
+                        'webhook': 'https://www.plaid.com/webhook'
+                    }
+                }).json()
+
+        response = requests.post(f'{sandbox_url}/item/public_token/exchange',
+            headers=standard_header,
+            json={
+                'client_id': client_id,
+                'secret': secret,
+                'public_token': response['public_token']
+            }).json()
+        
+        public_access_token = response['access_token']
+
+        user = User(user_id="1", created_at=datetime.now(), access_key=public_access_token,
+                    user_first_name="Steve", user_last_name="balls", user_email="steveballs@gmail.com",
+                    user_profile_picture=None)
+        
+        self.cur_user = user
+
+
 
     def tearDown(self):
         pass
+
+if __name__ == '__main__':
+    unittest.main()
