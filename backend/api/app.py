d2611f2dd45f2b1d4b3e997955508fe4686e9a83
abhinavbichal <65791148+thabat12@users.noreply.github.com>
2024-08-04 12:40:14 -0500

    working on api

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,16 +1,39 @@
-from flask import Flask
-# from api.routes.routes import subscription_blueprint
+from flask import Flask, jsonify, request
+from api.routes.routes import subscription_blueprint
 from api.config import Config
 from sqlalchemy import create_engine
-# from db.models import db
+from sqlalchemy.orm import Session
+from db.models import *
+import requests
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db_engine = create_engine()
 
-# db.init_app(app)
-
 # app.register_blueprint(subscription_blueprint, url_prefix='/api/subscriptions')
+@subscription_blueprint.route('/', methods=['GET'])
+def get_subscriptions():
+    subscriptions = Subscription.query.all()
+    return jsonify([subscription.to_dict() for subscription in subscriptions])
+
+@subscription_blueprint.route('/', methods=['POST'])
+def add_subscription():
+    data = request.get_json()
+    new_subscription = Subscription(
+        name=data['name'],
+        price=data['price'],
+        renewal_date=data['renewal_date']
+    )
+    return jsonify(new_subscription.to_dict()), 201
+
+@subscription_blueprint.route('/<int:id>', methods=['DELETE'])
+def delete_subscription(id):
+    subscription = Subscription.query.get_or_404(id)
+    return '', 204
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- backend/api/routes/routes.py
+++ backend/api/routes/routes.py
@@ -3,26 +3,3 @@ from backend.db.models import db, Subscription
 
 subscription_blueprint = Blueprint('subscriptions', __name__)
 
-@subscription_blueprint.route('/', methods=['GET'])
-def get_subscriptions():
-    subscriptions = Subscription.query.all()
-    return jsonify([subscription.to_dict() for subscription in subscriptions])
-
-@subscription_blueprint.route('/', methods=['POST'])
-def add_subscription():
-    data = request.get_json()
-    new_subscription = Subscription(
-        name=data['name'],
-        price=data['price'],
-        renewal_date=data['renewal_date']
-    )
-    db.session.add(new_subscription)
-    db.session.commit()
-    return jsonify(new_subscription.to_dict()), 201
-
-@subscription_blueprint.route('/<int:id>', methods=['DELETE'])
-def delete_subscription(id):
-    subscription = Subscription.query.get_or_404(id)
-    db.session.delete(subscription)
-    db.session.commit()
-    return '', 204
